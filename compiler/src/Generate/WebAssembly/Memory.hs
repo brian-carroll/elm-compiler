85cac01949fc9db2bee2dbd7dd82c72371cb7ad5
Brian Carroll <brian.carroll.ireland@gmail.com>
2018-06-02 16:28:53 +0100

    Get rid of MemoryObject typeclass, rearrange ExprMemory

--- compiler/src/Generate/WebAssembly/Memory.hs
+++ compiler/src/Generate/WebAssembly/Memory.hs
@@ -2,6 +2,8 @@ module Generate.WebAssembly.Memory where
 
   import Data.Word (Word8, Word32, Word64)
 
+  -- rename this
+  -- HeapObject, HeapValue, MemoryStructure, RuntimeObject, RuntimeValue, HeapStruct
   data ExprMemory
     = Bool Boolean
     | Chr Word32
@@ -16,13 +18,13 @@ module Generate.WebAssembly.Memory where
     | VarDebug
     | VarKernel Pointer
     | List Pointer Pointer -- head tail
-    | Function Arity Env Pointer -- arity env body
+    | Function Arity Pointer Pointer -- arity env body
     | Call NameDeref Arity [NameDeref] -- func argsLen args
     | TailCall NameDeref Arity [NameDeref] -- func argsLen args
-    | If Length8 [(Pointer, Pointer)] Pointer -- numIfElseIfs ifElseIfs finalElse
+    | If Pointer Length8 [(Pointer, Pointer)] -- finalElse numIfElseIfs ifElseIfs
     | Let Boolean EnvIndex Pointer Pointer -- recursive envIdx letBody inBody
     | Destruct NameDeref Length8 [Word32]  -- root numIndices indices
-    --      label root      decider         jumps
+    --        label    root      decider         jumps
     -- | Case N.Name N.Name (Decider Choice) [(Int Expr)]
     -- How to handle Case?
     | Accessor RecordFieldIdx -- fieldNameIdx
@@ -30,130 +32,95 @@ module Generate.WebAssembly.Memory where
     | Update Pointer Length32 [(RecordFieldIdx, Pointer)] -- record numFields [()]
     | Record Length32 [(RecordFieldIdx, Pointer)]
     | Unit
-    | Tuple Pointer Pointer Pointer
+    | Tuple2 Pointer Pointer
+    | Tuple3 Pointer Pointer Pointer
     | Shader
+    -- Env
+    -- Decider
+
+
+    {-
+      data Decider a
+        = Leaf a
+        | Chain
+            { _testChain :: [(DT.Path, DT.Test)]
+            , _success :: Decider a
+            , _failure :: Decider a
+            }
+        | FanOut
+            { _path :: DT.Path
+            , _tests :: [(DT.Test, Decider a)]
+            , _fallback :: Decider a
+            }
+
+      data Choice
+        = Inline Expr
+        | Jump Int
+      
+
+      So here's my understanding so far
+      - a decider is generated from a Case expression
+      - a leaf is an expression you evaluate when you make the decision
+      - 'inline' means the expression is generated at the leaf
+      - 'jump' means you first jump outwards to reach the expression
+          - JS is a labelled "break", Wasm can be similar
+      - a path is a sequence of accesses ('stuff.thing' in JS) to get to a value
+      - a test is a pattern to match agains the value we found on the path
+          - (ctor, cons, nil, tuple   are just structural matching
+              int, chr, str, bool   compare against values
+          - we don't have ctor's anywhere yet, they are Nodes rather than Exprs
+          - in prod mode, 0.19 has ctorToInt (see generateIfTest in Expression.hs)
+          - a chain generates a load of 'if's
+      - fanouts generate JS case statements
+          Wasm br_table
+
+        Case
+          label   dunno, probably for jumping out of loops
+          root    The root object we're matching against!!! of course!
+          decider 
+          jumps
+
+
+      What does this mean for Elm Wasm?
+        - paths! how do we do those?
+          - a sequence of accesses, simliar to destruct
+          - except no need to go outwards through Envs
+          - always matching against the root object
+                case root of
+                  Thing a 1 -> foo
+                  Stuff -> bar
+          - compile each case expression into a custom eval function
+          - pass in a pointer to the root object
+          - leaf expressions can refer to outer scopes
+          - but we don't have to evaluate the leaf expr,
+              just pass it back to 'eval'
+          - 'Case' data structure contains a function reference,
+              which is the size of a pointer
+      
+      Case Pointer FunctionId
+
+    -}
+
 
   type Length8 = Word8
   type Length32 = Word32
 
   type Boolean = Word8
-  type Pointer = Word32
   type Arity = Word8
-  type RecordFieldIdx = Word32
   type EnvIndex = Word32
 
+  type RecordFieldIdx = Word32
+  type Pointer = Word32
+  type ExprType = Word8
+
+
+  -- This should probably be its own expression constructor
+  -- takes a bit of evaluating
   data NameDeref =
     NameDeref Word8 Word8 -- scopes outward, env index
 
   data Env =
     Env Pointer Length32 [Pointer] -- parent length values
-
-
-
-  class MemoryObject a where
-    bytes :: a -> Int
-
-  instance MemoryObject Word8 where
-    bytes _ = 1
-
-  instance MemoryObject Word32 where
-    bytes _ = 4
-
-  instance MemoryObject Word64 where
-    bytes _ = 8
-
-  instance MemoryObject NameDeref where
-    bytes (NameDeref a b) = bytes a + bytes b
-
-  instance MemoryObject a => MemoryObject [a] where
-    bytes [] = 0
-    bytes (h : t) = bytes h + bytes t
-
-  instance (MemoryObject a, MemoryObject b) => MemoryObject (a, b) where
-    bytes (a , b) = bytes a + bytes b
-
-  instance MemoryObject Env where
-    bytes (Env parent length values) =
-      bytes parent + bytes length + bytes values
-
-  instance MemoryObject ExprMemory where
-    bytes expr =
-      1 + case expr of
-        Bool x ->
-          bytes x
-        Chr x ->
-          bytes x
-        Str len list ->
-          bytes len + bytes list
-        Int x ->
-          bytes x
-        Float x ->
-          bytes x
-        VarLocal x ->
-          bytes x
-        VarGlobal x ->
-          bytes x
-        VarEnum enumIdx idx ->
-          bytes enumIdx + bytes idx
-        VarBox x ->
-          bytes x
-        VarCycle x ->
-          bytes x
-        VarDebug ->
-          0
-        VarKernel x ->
-          bytes x
-        List h t ->
-          bytes h + bytes t
-        Function arity env bodyptr ->
-          bytes arity + bytes env + bytes bodyptr
-        Call fun arity args ->
-          bytes fun + bytes arity + bytes args
-        TailCall fun arity args ->
-          bytes fun + bytes arity + bytes args
-        If len ifElseIfs finalElse ->
-          bytes len + bytes ifElseIfs + bytes finalElse
-        Let recursive envIdx letBody inBody ->
-          bytes recursive + bytes envIdx + bytes letBody + bytes inBody
-        Destruct name len path ->
-          bytes name + bytes len + bytes path
-        -- Case
-        Accessor field ->
-          bytes field
-        Access record field ->
-          bytes record + bytes field 
-        Update record len pairs ->
-          bytes record + bytes len + bytes pairs
-        Record len pairs ->
-          bytes len + bytes pairs
-        Unit ->
-          0
-        Tuple x y z ->
-          bytes x + bytes y + bytes z
-        Shader ->
-          0
-
-  {-
-    Decider Choice
-      Leaf Choice
-      Chain
-        { _testChain
-        , _success
-        , _failure
-        }
-      Fanout
-
-    Choice
-      Inline
-      Jump
-  -}
-
-
-
-
-
-
-
 
   data MemoryLayout =
     MemoryLayout
