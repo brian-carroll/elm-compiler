5828cd1fcd33a48d34c9c95f1fb9aa2064f1b978
Brian Carroll <brian.carroll.ireland@gmail.com>
2018-08-09 08:02:15 +0100

    Modify run.sh script for GHC 8.0

--- run.sh
+++ run.sh
@@ -3,14 +3,8 @@
 # exit when any command fails
 set -e
 
-mv ui/browser/elm-stuff /tmp/elm-stuff
-mv ~/.elm /tmp/.elm
-
 # If at first you don't succeed, try, try again
-cabal build || cabal build
-
-mv /tmp/elm-stuff ui/browser/elm-stuff
-mv /tmp/.elm ~/.elm
+cabal build
 
 cd ui/browser
 ./elm make src/Index.elm --output elm.wat
