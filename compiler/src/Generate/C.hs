0abe6594b517b7df8fb47d45b3fd73892406c037
Brian Carroll <brian.carroll.ireland@gmail.com>
2019-07-20 17:06:40 +0100

    Define generator state and refactor

--- compiler/src/Generate/C.hs
+++ compiler/src/Generate/C.hs
@@ -6,13 +6,17 @@ module Generate.C
     
 import Prelude hiding (cycle, print)
 import qualified Data.ByteString.Builder as B
--- import Data.Monoid ((<>))
+import Data.Monoid ((<>))
 -- import qualified Data.List as List
 -- import Data.Map ((!))
 import qualified Data.Map as Map
 import qualified Data.Name as Name
--- import qualified Data.Set as Set
+import qualified Data.Set as Set
 -- import qualified Data.Utf8 as Utf8
+import qualified Language.C as C
+import qualified Language.C.Data.Name as C
+import qualified Language.C.Pretty as C
+import qualified Text.PrettyPrint as PP
 
 -- import qualified AST.Canonical as Can
 import qualified AST.Optimized as Opt
@@ -27,10 +31,6 @@ import qualified Elm.ModuleName as ModuleName
 -- import qualified Reporting.Doc as D
 -- import qualified Reporting.Render.Type as RT
 -- import qualified Reporting.Render.Type.Localizer as L
-import qualified Language.C as C
-import qualified Language.C.Data.Name as C
-import qualified Language.C.Pretty as C
-import qualified Text.PrettyPrint as PP
 
 
 
@@ -42,28 +42,83 @@ type Graph = Map.Map Opt.Global Opt.Node
 type Mains = Map.Map ModuleName.Canonical Opt.Main
 
 
-makeFieldIdent :: Name.Name -> C.Name -> C.Ident
-makeFieldIdent field nodeName =
-    C.mkIdent C.nopos ("RecField_" ++ Name.toChars field) nodeName
+-- GRAPH TRAVERSAL STATE
+
+data State =
+  State
+    { _seenGlobals :: Set.Set Opt.Global
+    , _revKernels :: [B.Builder]
+    , _revBuildersJS :: [B.Builder]
+    , _revBuildersC :: [B.Builder]
+    , _fields :: Set.Set Name.Name
+    , _ctors :: Set.Set Name.Name
+    }
+
+
+emptyState :: State
+emptyState =
+  State
+    { _seenGlobals = Set.empty
+    , _revKernels = []
+    , _revBuildersJS = []
+    , _revBuildersC = []
+    , _fields = Set.empty
+    , _ctors = Set.empty
+    }
+
+
+-- Language.C AST uses unique integer IDs (confusingly called Name)
+-- It's for fast equality, but we're not parsing so we don't need that
+dummyNodeId :: C.Name
+dummyNodeId =
+  C.Name 0
+
+
+identFromChars :: [Char] -> C.Ident
+identFromChars chars =
+  C.mkIdent C.nopos chars dummyNodeId
+
+
+identWithPrefix :: [Char] -> Name.Name -> C.Ident
+identWithPrefix prefix name =
+  identFromChars (prefix ++ Name.toChars name)
+
+
+identFromFieldName :: Name.Name -> C.Ident
+identFromFieldName field =
+  identWithPrefix "Field_" field
 
 
 generate :: Opt.GlobalGraph -> Mains -> B.Builder
-generate (Opt.GlobalGraph graph fields) mains =
+generate (Opt.GlobalGraph graph fieldFreqMap) mains =
   let
-    -- Language.C requires each node to have a unique "name" which is actually an Int
-    enumIdent :: C.Ident
-    enumIdent =
-      C.mkIdent C.nopos "ElmRecordField" (C.Name 0)
+    fields :: Set.Set Name.Name
+    fields = Map.keysSet fieldFreqMap
 
-    fieldEnumIdents :: [C.Ident]
-    fieldEnumIdents =
-      zipWith makeFieldIdent (Map.keys fields) (C.namesStartingFrom 1)
+    stateWithFields :: State
+    stateWithFields = emptyState { _fields = fields }
+  in
+    generateFieldEnum stateWithFields
+
+
+generateFieldEnum :: State -> B.Builder
+generateFieldEnum state =
+  let
+    fields :: Set.Set Name.Name
+    fields = _fields state
+
+    identsAndValues :: [(C.Ident, Maybe (C.CExpression C.NodeInfo))]
+    identsAndValues =
+      Set.foldr
+        (\f idsAndVals -> (identFromFieldName f, Nothing) : idsAndVals)
+        []
+        fields
 
     fieldEnum :: C.CEnumeration C.NodeInfo
     fieldEnum =
       C.CEnum
-        (Just enumIdent)
-        (Just $ map (\i -> (i, Nothing)) fieldEnumIdents)
+        (Just $ identFromChars "ElmRecordField")
+        (Just $ identsAndValues)
         []
         C.undefNode
 
