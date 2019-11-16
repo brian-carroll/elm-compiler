d41f12268e8ed274bf1fe97af3b95fa52167141b
Brian Carroll <brian.carroll.ireland@gmail.com>
2019-11-16 22:25:28 +0000

    C.Name compiling, errors elsewhere

--- compiler/src/Generate/C/Name.hs
+++ compiler/src/Generate/C/Name.hs
@@ -1,35 +1,42 @@
 {-# OPTIONS_GHC -Wall #-}
 {-# LANGUAGE OverloadedStrings #-}
 module Generate.C.Name
-( Name
-, local
-, global
-, globalInitFn
-, globalInitPtr
-, jsKernelVal
-, cKernelVal
-, ctor
-, field
-, fieldGroup
-, evaluator
-, literalInt
-, literalFloat
-, literalString
-, literalChar
-)
-where
+  ( Name
+  , toBuilder
+  , local
+  , global
+  , globalInitFn
+  , globalInitPtr
+  , jsKernelValue
+  , cKernelValue
+  , cycleVar
+  , ctorId
+  , fieldId
+  , fieldGroup
+  , globalEvaluator
+  , localEvaluator
+  , literalInt
+  , literalFloat
+  , literalString
+  , literalChar
+  , unit
+  , true
+  , false
+  )
+  where
 
 import qualified Data.ByteString.Builder as B
 import Data.Monoid ((<>))
-import qualified Data.Map as Map
 import qualified Data.Name as Name
-import qualified Data.Set as Set
 import qualified Data.Utf8 as Utf8
 import Data.Word (Word8)
+import qualified Data.List as List
+import qualified Data.Char as Char
 
-import qualified Data.Index as Index
 import qualified Elm.ModuleName as ModuleName
 import qualified Elm.Package as Pkg
+import qualified Elm.String as ES
+import qualified Elm.Float as EF
 
   
 -- NAME
@@ -72,9 +79,11 @@ localEvaluator home name index =
   Name ("eval_" <> globalBuilder home name <> "_lambda" <> B.intDec index)
 
 
-cycle :: ModuleName.Canonical -> Name.Name -> Name
-cycle home name
-  Name $ builderFromUtf8List $ homeToList (home ++ ["cyclic", name])
+cycleVar :: ModuleName.Canonical -> Name.Name -> Name
+cycleVar home name =
+  Name $ mconcat $
+    (homeToBuilders home)
+    ++ ["cyclic", Name.toBuilder name]
 
 
 jsKernelValue :: Name.Name -> Name.Name -> Name
@@ -102,24 +111,26 @@ fieldGroup fields =
   Name $ "fg_" <> builderFromUtf8List fields
 
 
-literalInt :: Int -> B.Builder
+literalInt :: Int -> Name
 literalInt x =
   Name $ "literal_int_" <> B.intDec x
 
 
-literalFloat :: EF.Float -> B.Builder
+literalFloat :: EF.Float -> Name
 literalFloat x =
   Name $ "literal_float_" <> (builderFromUtf8List $ Utf8.split dot x)
 
 
 literalString :: ES.String -> Name
 literalString s =
-  Name $ "literal_string_" ++ (concatMap escapeChar $ ES.toChars s)
+  Name $ mconcat $
+    "literal_string_" : (map escapeChar $ ES.toChars s)
 
 
 literalChar :: ES.String -> Name
 literalChar s =
-  Name $ "literal_char_" ++ (concatMap escapeChar $ ES.toChars s)
+  Name $ mconcat $
+    "literal_char_" : (map escapeChar $ ES.toChars s)
 
 
 -- INTERNAL UTILS
@@ -127,28 +138,34 @@ literalChar s =
 
 escapeChar :: Char -> B.Builder
 escapeChar c =
-  if Char.isAscii c && (Char.isAlphaNum c || c == '_') then
-    B.char8 c
+  if Char.isAscii c then
+    if Char.isAlphaNum c then
+      B.char8 c
+    else
+      B.char8 '_'
   else
-    "\\U" ++ (B.int32HexFixed $ fromIntegral $ Char.ord c)
+    "\\U" <> (B.int32HexFixed $ fromIntegral $ Char.ord c)
 
 
-globalBuilder :: ModuleName.Canonical -> Name.Name -> Name
+globalBuilder :: ModuleName.Canonical -> Name.Name -> B.Builder
 globalBuilder home name =
-  builderFromUtf8List (homeToList home ++ [name])
+  mconcat (homeToBuilders home ++ [Name.toBuilder name])
 
 
 builderFromUtf8List :: [Utf8.Utf8 t] -> B.Builder
 builderFromUtf8List names =
   mconcat $ List.intersperse "_" $
-    map Name.toBuilder names
+    map Utf8.toBuilder names
 
 
-homeToList :: ModuleName.Canonical -> [Name.Name]
-homeToList (ModuleName.Canonical (Pkg.Name author project) mod) =
-  Utf8.split hyphen author
-  ++ Utf8.split hyphen project
-  ++ Utf8.split dot mod
+-- these Utf8 mofos all have different phantom types
+-- need to convert toBuilder first then shove into a list
+homeToBuilders :: ModuleName.Canonical -> [B.Builder]
+homeToBuilders (ModuleName.Canonical (Pkg.Name author project) modul) =
+  [ builderFromUtf8List $ Utf8.split hyphen author
+  , builderFromUtf8List $ Utf8.split hyphen project
+  , builderFromUtf8List $ Utf8.split dot modul
+  ]
 
 
 hyphen :: Word8
