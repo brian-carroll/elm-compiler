688e7c74b33c222ae539438804e5a64bd85c2427
Brian Carroll <brian.carroll.ireland@gmail.com>
2021-01-16 11:21:45 +0000

    further dev of bytecode AST and state

--- compiler/src/Generate/ByteCode.hs
+++ compiler/src/Generate/ByteCode.hs
@@ -4,25 +4,73 @@ module Generate.ByteCode
   )
   where
 
+{-
+  A common Intermediate Representation for targets like WebAssembly, LLVM, or C.
+  It knows all the byte-level details of the program.
+
+  Short term:    slowly refactor parts of the C generator to use ByteCode
+  Medium term:   generate all C via ByteCode
+  Long term:     create LLVM backend
+-}
+
+import qualified Data.IntMap as IntMap
+import qualified Data.Map as Map
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Builder as B
+import qualified Data.Name as N
 
 import qualified AST.Optimized as Opt
 import qualified Elm.ModuleName as ModuleName
 import qualified Generate.ByteCode.Name (Name)
+import qualified Generate.JavaScript as JS
+
+data BcGenState =
+  BcGenState
+    { stateLiterals :: BcGenLiterals
+    , stateGlobals :: Map.Map Opt.Global Global -- cache
+    , stateProgram :: Program
+    , stateJs :: JS.State
+    }
+
+data BcGenLiterals =
+  BcGenLiterals
+    { litInt :: Set.Set Int
+    , litFloat :: Set.Set EF.Float
+    , litChr :: Set.Set ES.String
+    , litStr :: Set.Set ES.String
+    , litAccessor :: Set.Set N.Name
+    , litFieldGroup :: Set.Set [N.Name]
+    , litField :: Set.Set N.Name
+    , litCtor :: Set.Set N.Name
+    , litJsKernel :: Set.Set N.Name N.Name
+    , litPorts :: Set.Set Opt.Global -- JS values with global name format
+    }
 
 
 data Program =
   Program
-    { progConstants :: Map.Map Name ConstValue
-    , progEvalFuncs :: Map.Map Name Procedure
-    , progGcRootInits :: Map.Map Name Procedure
-    , progMains :: [Name]
+    { progLiterals :: [(Name, ConstValue)]
+    , progGlobals :: [Global]    -- dependency order
+    , progRoots :: [Name]        -- for init
+    , progKernels :: [Name]      -- for linking to JS
+    , progMains :: [Name]        -- for linking to JS
     }
 
+data Global
+  = GlobalConst Name ConstValue
+  | GlobalRoot Procedure
+  | GlobalKernel Name Name
+
 data ConstValue
-  = ConstStruct TypeTag BS.ByteString [ConstValue]
-  | ConstUnboxed Int
+  = ConstStruct TypeTag [StructBody] [ConstValue]
+  | ConstClosure Int [ConstValue] -- arity, pre-applied args
+  | ConstData [StructBody]
+  | ConstRef Name
+
+data StructBody
+  = StructInt32 Int
+  | StructInt16 Int
+  | StructFloat EF.Float
 
 data TypeTag
   = ElmInt
@@ -41,7 +89,8 @@ data TypeTag
 data Procedure =
   Procedure
     { evalName :: B.Builder
-    , evalArgs :: Int
+    , evalArgs :: [Name]
+    , evalLocals :: [Name]
     , evalInstructions :: [Instruction]
     }
 
@@ -50,25 +99,25 @@ data Instruction
   | LoadEvalFunc Name
   | LoadLocal Name
   | LoadGcRoot Name
+  | LoadBody StructInfo Int Instruction
   | LoadChild StructInfo Int Instruction
-  | LoadField Name Instruction
   | StoreLocal Name Instruction
   | StoreGcRoot Name Instruction
+  | StoreBody StructInfo Int Instruction Instruction
   | StoreChild StructInfo Int Instruction Instruction
-  | StoreField Name Instruction Instruction
   | Copy Instruction
   | Construct TypeTag BS.ByteString [Instruction]
-  | Apply Instruction [Instruction]
+  | CallProc Instruction [Instruction]
   | Block LabelId Instruction
   | ExitBlock LabelId
   | TestTypeTag TypeTag Instruction
+  | FindFieldPos FieldId Instruction
   | NumOpBinary NumType NumOpBinary Instruction Instruction
   | NumOpUnary NumType NumOpUnary Instruction
   | NumOpConvert NumType NumType Instruction
 
 
-newtype LabelId =
-  LabelId Int
+newtype LabelId = LabelId Int
 
 
 data StructInfo =
